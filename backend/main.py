0a435d4d77b5012bf7a59ac23a1a57f86c86f635
JULIA CARLA FERREIRA PIMENTEL <202451069845@alunos.newtonpaiva.edu.br>
2026-05-25 20:23:54 -0300

    implementa frontend assíncrono com CRUD

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.filme_routes import router as filme_router
 from routes.avaliacao_routes import router as avaliacao_router
 
@@ -8,8 +10,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(filme_router)
 app.include_router(avaliacao_router)
+
 
 @app.get("/")
 def home():
